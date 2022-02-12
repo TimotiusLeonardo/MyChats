44cdfd8b9637835c61f6021cfc1b9eceffc3f6ba
Timotius Leonardo <lionato14@gmail.com>
2022-02-12 17:06:46 +0700

    Register method able to save profile picture
    1. Add register functionality to be able to upload picture
    2. Still need to handle app picture change when segmented controller updated to login
    3. Disable Button when async function is running

--- MyChats/Controller/LoginController.swift
+++ MyChats/Controller/LoginController.swift
@@ -97,6 +97,8 @@ class LoginController: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
+        profileImageView.isUserInteractionEnabled = true
+        profileImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleSelectProfileImageView)))
         hideKeyboardWhenTappedAround()
         
         view.addSubview(inputContainerView)
@@ -188,89 +190,5 @@ class LoginController: UIViewController {
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
-    }
-    
-    @objc func handleLoginRegister() {
-        if loginRegisterSegmentedControl.selectedSegmentIndex == 0 {
-            handleLogin()
-        } else {
-            handleRegister()
-        }
-    }
-    
-    func handleLogin() {
-        guard let email = emailTextField.text, let password = passwordTextField.text else {
-            print("Form is not valid")
-            return
-        }
-        
-        Auth.auth().signIn(withEmail: email,
-                           password: password) { user, error in
-            if error != nil {
-                print(error?.localizedDescription ?? "Error Sign in With Firebase")
-                return
-            }
-            
-            self.dismiss(animated: true, completion: nil)
-        }
-    }
-    
-    func handleRegister() {
-        guard let email = emailTextField.text, let password = passwordTextField.text, let name = nameTextField.text else {
-            return
-        }
-        Auth.auth().createUser(withEmail: email, password: password) { result, error in
-            if let error = error {
-                print(error.localizedDescription)
-                return
-            }
-            
-            guard let uid = result?.user.uid else {
-                return
-            }
-            
-            // Succes Register User
-            let ref = Database.database().reference(fromURL: "https://mychats-cad9d-default-rtdb.asia-southeast1.firebasedatabase.app")
-            let userParentRef = ref.child("users").child(uid)
-            let values = ["name": name, "email": email]
-            userParentRef.updateChildValues(values) { error, ref in
-                if let error = error {
-                    print(error.localizedDescription)
-                    return
-                }
-                
-                self.dismiss(animated: true, completion: nil)
-            }
-        }
-    }
-    
-    @objc func handleSegmentValueChanged() {
-        let title = loginRegisterSegmentedControl.titleForSegment(at: loginRegisterSegmentedControl.selectedSegmentIndex)
-        loginRegisterButton.setTitle(title, for: .normal)
-        
-        // Change Heigth of container View
-        inputsContainerViewHeightAnchor?.constant = loginRegisterSegmentedControl.selectedSegmentIndex == 0 ? 100 : 150
-        nameTextFieldHeightAnchor?.isActive = false
-        passwordTextFieldHeightAnchor?.isActive = false
-        emailTextFieldHeightAnchor?.isActive = false
-        
-        nameTextFieldHeightAnchor = nameTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor,
-                                                                          multiplier: loginRegisterSegmentedControl.selectedSegmentIndex == 0 ? 0 : 1/3)
-        passwordTextFieldHeightAnchor = passwordTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor,
-                                                                                  multiplier: loginRegisterSegmentedControl.selectedSegmentIndex == 0 ? 1/2 : 1/3)
-        emailTextFieldHeightAnchor = emailTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor,
-                                                                            multiplier: loginRegisterSegmentedControl.selectedSegmentIndex == 0 ? 1/2 : 1/3)
-        nameTextFieldHeightAnchor?.isActive = true
-        passwordTextFieldHeightAnchor?.isActive = true
-        emailTextFieldHeightAnchor?.isActive = true
-        
-        // Animate Constraint Change
-        UIView.animate(withDuration: 0.2,
-                       delay: 0, options: .curveLinear) {
-            self.view.layoutIfNeeded()
-        } completion: { _ in
-            //
-        }
-        
     }
 }
